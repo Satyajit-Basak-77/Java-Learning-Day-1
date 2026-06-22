d1fea1d0a539a43959d256fc2c9cec63fac32cad
Satyajit-Basak-77 <satyajitbasak1832@gmail.com>
2026-06-22 20:33:37 +0530

    codded on my own using variables

---
+++ quiz.java
@@ -0,0 +1,8 @@
+public class quiz {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+        int ans = (a*b)/(a-b);
+        System.out.print("Your answer is: " + ans);
+    }   
+}
